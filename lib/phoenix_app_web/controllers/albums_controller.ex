e8f7e908f53a02c892988a781bd1044b8e1dd739
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-11-19 21:23:04 -0700

    Add intial albums page

---
+++ lib/phoenix_app_web/controllers/albums_controller.ex
@@ -0,0 +1,7 @@
+defmodule PhoenixAppWeb.AlbumsController do
+  use PhoenixAppWeb, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+end
---
+++ lib/phoenix_app_web/views/albums_view.ex
@@ -0,0 +1,3 @@
+defmodule PhoenixAppWeb.AlbumsView do
+  use PhoenixAppWeb, :view
+end
